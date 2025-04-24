9823f4492a80b6e2b0ebf69e6f37c74debe32d20
sheena-chacko <sheenachacko57@gmail.com>
2025-04-24 17:09:10 +0530

    edit report

--- Frontend/src/Pages/AdminReports.jsx
+++ Frontend/src/Pages/AdminReports.jsx
@@ -6,16 +6,17 @@ import FinancialReports from "../Pages/FinancialReports";
 import ViewExpense from "./ViewExpense";
 import ViewIncome from "./ViewIncome";
 import ViewBalanceSheet from "./ViewBalanceSheet";
+import DonationChart from "./DonationChart";
 
 const AdminReports = () => {
   const [showContent, setShowContent] = useState(null);
 
   const reports = [
-    { id: 1, title: "Monthly Donations Report", date: "February 2025", details: "Total donations collected: $5,320" },
-    { id: 2, title: "Expense Reports", date: "March 2025", details: "Total attendees: 240" },
-    { id: 3, title: "Financial Reports", date: "January 2025", details: "Total participants: 85" },
-    { id: 4, title: "Balance Sheet Report", date: "Year 2024", details: "Total Income: $50,000 | Expenses: $35,000 | Savings: $15,000" },
-    { id: 5, title: "Income Reports", date: "March 2025", details: "Total attendees: 240" },
+    { id: 1, title: "piechart", date: "February 2025", details: "Total donations collected" },
+    { id: 2, title: "Expense Reports", date: "March 2025", details: "Total expenses" },
+    { id: 3, title: "Financial Reports", date: "January 2025", details: "Total financial" },
+    { id: 4, title: "Balance Sheet Report", date: "Year 2024", details: "Total Income | Expenses | Savings" },
+    { id: 5, title: "Income Reports", date: "March 2025", details: "Total income" },
   ];
 
   const handleShowContent = (contentType) => {
@@ -24,6 +25,8 @@ const AdminReports = () => {
 
   return (
     <div className="p-6 bg-white shadow rounded-lg">
+            {showContent === "DonationChart" && <DonationChart />}
+
       {showContent === "BalanceSheet" && <ViewBalanceSheet />}
       {showContent === "ExpenseManagement" && <ViewExpense />}
       {showContent === "IncomeStatements" && <ViewIncome />}
@@ -43,6 +46,14 @@ const AdminReports = () => {
                 <h3 className="text-xl font-semibold text-yellow-700">{report.title}</h3>
                 <p className="text-gray-600">📅 {report.date}</p>
                 <p className="text-gray-700 mt-2">{report.details}</p>
+                {report.title === "Donation Chart" && (
+                  <button
+                    onClick={() => handleShowContent("DonationChart")}
+                    className="mt-3 inline-block bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                  >
+                    Pie Chart
+                  </button>
+                )}
 
                 {report.title === "Balance Sheet Report" && (
                   <button
